050918d6e06517eba10b43a7da8e1e29ed739eb3
Sergio Marelli <sergio.mrll@gmail.com>
2020-06-13 15:48:32 +0100

    removed validation to allow creationg of delegation without :kind

--- app/models/delegation.rb
+++ app/models/delegation.rb
@@ -2,6 +2,6 @@ class Delegation < ActiveRecord::Base
 	belongs_to(:pool_epoch)
 	belongs_to(:user)
 
-	validates :kind, presence: ['wished', 'delegated', nil]
+	# validates :kind, presence: ['wished', 'delegated', nil]
 	validates_uniqueness_of :kind, scope: [:user_id, :pool_epoch_id]
 end
